21341b7bc2651ba003d01218c8796648352ed865
niklashansson <niklas@bloomweb.co>
2023-09-16 20:00:37 +0200

    Updates

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import { companyMetrics } from '$utils/companyMetrics';
 import { deals } from '$utils/deals';
-import { initListings } from '$utils/listings/controller';
 
 import { swiper } from './utils/swiper';
 
@@ -11,4 +10,4 @@ window.Webflow.push(() => {
   companyMetrics();
 });
 
-/*<script defer src="https://cdn.jsdelivr.net/gh/niklashansson/tentacle.se@36bc3081b5923f773e988517361fd4dfac8fcb0b/dist/index.min.js"></script>*/
+/*<script defer src="https://cdn.jsdelivr.net/gh/niklashansson/tentacle.se@b0409f150384adf5b01d059a44544356f816cea6/dist/index.min.js"></script>*/
--- src/utils/deals.ts
+++ src/utils/deals.ts
@@ -33,6 +33,7 @@ export const deals = async function () {
     });
   }
 
+  // Table component attribute set in Webflow - limit amount of deals rendered
   function getDealsLimit() {
     const limit = tableEl?.getAttribute('tentacle-deals-limit');
 
@@ -51,6 +52,7 @@ export const deals = async function () {
     return number;
   }
 
+  // Fetch deals
   async function fetchDeals(lang: string) {
     try {
       const res = await fetch('https://app.tentacle.se/api/public-deals', {
@@ -67,4 +69,112 @@ export const deals = async function () {
       console.error(err);
     }
   }
+
+  async function fetchDeal(id: string, lang: string) {
+    try {
+      const res = await fetch(`https://app.tentacle.se/api/public-deals/${id}`, {
+        headers: {
+          referrer: 'https://app.tentacle.se',
+          'Content-Type': 'application/javascript',
+          'Accept-Language': lang,
+        },
+      });
+      const { data } = await res.json();
+
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  const modalList = document.querySelector('[tentacle-modal-element="list"]');
+  const modalComponent = document.querySelector('[tentacle-modal-element="component"]');
+  if (!modalList || !modalComponent) return;
+
+  async function renderModal(id: string) {
+    clearModalList();
+
+    // Get deal from tentacle API
+    const deal = await fetchDeal(id, lang);
+
+    // Prepare details + titles in array
+    const specs = dealSpecs(deal);
+
+    // Render details to modal
+    appendSpecs(specs);
+
+    // Display modal
+    displayModal();
+  }
+
+  function displayModal() {
+    modalComponent.style.display = 'flex';
+    modalComponent.style.opacity = '1';
+  }
+
+  function appendSpecs(specs) {
+    specs.forEach((spec) => {
+      if (!spec.value && spec.show) return;
+
+      const div = document.createElement('div');
+      div.classList.add('deal-modal_item');
+
+      div.innerHTML = `<p class="text-color-grey">${spec.title}</p>
+      ${
+        spec.show
+          ? `<p class="text-size-medium">${spec.value}</p>`
+          : '<div class="skeleton-text is-listing-2"></div>'
+      }`;
+
+      modalList?.appendChild(div);
+    });
+  }
+
+  function dealSpecs(deal) {
+    const values = [];
+
+    const geography = deal.county;
+    const geographyTitle = lang === 'sv' ? 'Geografi' : 'Geography';
+
+    const propertyType = deal.property_types ? deal.property_types.join(', ') : undefined;
+    const propertyTypeTitle = lang === 'sv' ? 'Fastighetsslag' : 'Property Type';
+
+    const indicativeYield = deal.yield_range ? deal.yield_range.join(' - ') + '%' : undefined;
+    const indicativeYieldTitle = lang === 'sv' ? 'Indikativ Yield' : 'Indicative Yield';
+
+    const operatingNet = undefined;
+    const operatingNetTitle = lang === 'sv' ? 'Driftnetto' : 'Operating Net';
+
+    const exchangeableArea = undefined;
+    const exchangeableAreaTitle = lang === 'sv' ? 'Utbytbar Area' : 'Exchangeable Area';
+
+    values.push(
+      { title: geographyTitle, value: geography, show: true },
+      { title: propertyTypeTitle, value: propertyType, show: true },
+      { title: indicativeYieldTitle, value: indicativeYield, show: true },
+      { title: operatingNetTitle, value: operatingNet, show: false },
+      { title: exchangeableAreaTitle, value: exchangeableArea, show: false }
+    );
+
+    return values;
+  }
+
+  function clearModalList() {
+    modalList.innerHTML = '';
+  }
+
+  // function appendDealModal(deal) {
+  //   const div = document.createElement('div');
+  //   div.classList.add('deal-modal_item');
+
+  // }
+
+  // Deal modal
+  tableEl.addEventListener('click', (e) => {
+    const row = e.target.closest('.deals-table_row');
+    if (!row) return;
+    const id = row.getAttribute('deal-id');
+
+    renderModal(id);
+  });
 };
